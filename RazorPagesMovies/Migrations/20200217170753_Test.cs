fcbde1ae229c11a7ced8d9f3b04ab6ab90063951
Gabriel <gabriel.justo@acad.pucrs.br>
2020-02-17 14:11:03 -0300

    Movie Page done

---
+++ RazorPagesMovies/Migrations/20200217170753_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovies.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Movie");
+        }
+    }
+}
